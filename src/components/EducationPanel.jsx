2404e828866a6cc8ff610bb449ad57e309518c25
Yengzzkie <gatchalian.manuel@gmail.com>
2024-05-04 16:49:08 -0400

    style/added some styling for mobile screens

--- src/components/EducationPanel.jsx
+++ src/components/EducationPanel.jsx
@@ -2,33 +2,28 @@ import '../styles/EducationPanel.css';
 
 export default function EducationPanel({ education, handleAddEducation, setEducation }) {
 
-  function handleFromChange(e, index) {
-    const updatedEducation = [...education];
-    updatedEducation[index] = { ...updatedEducation[index], from: e.target.value };
+  function handleFromChange(e, id) {
+    const updatedEducation = education.map(educ => educ.id === id ? {...educ, from: e.target.value} : educ);
     setEducation(updatedEducation);
   }
 
-  function handleToChange(e, index) {
-    const updatedEducation = [...education];
-    updatedEducation[index] = { ...updatedEducation[index], to: e.target.value };
+  function handleToChange(e, id) {
+    const updatedEducation = education.map(educ => educ.id === id ? {...educ, to: e.target.value} : educ);
     setEducation(updatedEducation);
   }
 
-  function handleSchoolChange(e, index) {
-    const updatedEducation = [...education];
-    updatedEducation[index] = { ...updatedEducation[index], school: e.target.value };
+  function handleSchoolChange(e, id) {
+    const updatedEducation = education.map(educ => educ.id === id ? {...educ, school: e.target.value} : educ);
     setEducation(updatedEducation);
   }
 
-  function handleFieldChange(e, index) {
-    const updatedEducation = [...education];
-    updatedEducation[index] = { ...updatedEducation[index], field: e.target.value };
+  function handleFieldChange(e, id) {
+    const updatedEducation = education.map(educ => educ.id === id ? {...educ, field: e.target.value} : educ);
     setEducation(updatedEducation);
   }
 
-  function handleDeleteEducation(index) {
-    const updatedEducation = [...education];
-    updatedEducation.splice(index, 1);
+  function handleDeleteEducation(id) {
+    const updatedEducation = education.filter(educ => educ.id !== id);
     setEducation(updatedEducation);
   }
 
@@ -36,11 +31,11 @@ export default function EducationPanel({ education, handleAddEducation, setEduca
     <div className="user-panel">
       {education.map((educ, index) => (
         <div key={index} className="education-panel">
-          <button className='delete-education' onClick={() => {handleDeleteEducation(index)}}><i className="fa-regular fa-trash-can"></i></button>
-          <input type="number" placeholder="From" value={educ.from} onChange={(e) => handleFromChange(e, index)} />
-          <input type="number" placeholder="To" value={educ.to} onChange={(e) => handleToChange(e, index)}/>
-          <input type="text" placeholder="School / University" value={educ.school} onChange={(e) => handleSchoolChange(e, index)} />
-          <input type="text" placeholder="Field of Study" value={educ.field} onChange={(e) => handleFieldChange(e, index)} />
+          <button className='delete-education' onClick={() => {handleDeleteEducation(educ.id)}}><i className="fa-regular fa-trash-can"></i></button>
+          <input type="number" placeholder="From" value={educ.from} onChange={(e) => handleFromChange(e, educ.id)} />
+          <input type="number" placeholder="To" value={educ.to} onChange={(e) => handleToChange(e, educ.id)}/>
+          <input type="text" placeholder="School / University" value={educ.school} onChange={(e) => handleSchoolChange(e, educ.id)} />
+          <input type="text" placeholder="Field of Study" value={educ.field} onChange={(e) => handleFieldChange(e, educ.id)} />
         </div>
       ))}
       <button onClick={handleAddEducation}>Add New Education</button>
--- src/components/MainPanel.jsx
+++ src/components/MainPanel.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { v4 as uuid } from "uuid";
 import PersonalPanel from "../components/PersonalPanel";
 // import PreviewPanel from "../components/PreviewPanel";
 import EducationPanel from "../components/EducationPanel";
@@ -6,25 +7,29 @@ import WorkPanel from "../components/WorkPanel";
 import "../styles/MainPanel.css";
 
 export default function MainPanel() {
+
+  //states
   const [name, setName] = useState("John Doe");
   const [email, setEmail] = useState("you@yourdomain.com");
   const [contact, setContact] = useState("123-456-7890");
-
-  const [education, setEducation] = useState([{from: '2008', to: '2012', school: 'University of Youtube', field: 'BS Nursing'}])
-  const [work, setWork] = useState([{from: '2013', to: '2015', company: 'Apple Inc', position: 'iTunes Support', description: 'I did some blah blah blah'}])
-
+  const [education, setEducation] = useState([{id: uuid(), from: '2008', to: '2012', school: 'University of Youtube', field: 'BS Nursing'}])
+  const [work, setWork] = useState([{id: uuid(), from: '2013', to: '2015', company: 'Apple Inc', position: 'iTunes Support', description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio, eveniet repellat! Labore earum ad sed iure a corrupti ratione tenetur quam. Sequi, magni quo. Et explicabo earum fuga illo iure.'}])
+  
+  //new object handlers
   function handleAddEducation() {
     let updatedEducation = [...education];
-    updatedEducation.push({from: '', to: '', school: '', field: ''})
+    updatedEducation.push({id: uuid(), from: '', to: '', school: '', field: ''})
     setEducation(updatedEducation)
   }
 
   function handleAddWork() {
     let updatedWork = [...work];
-    updatedWork.push({from: 'From', to: 'To', company: 'Company Name', position: 'Job Title / Position', description: 'Job Description'})
+    updatedWork.push({id: uuid(), from: 'From', to: 'To', company: 'Company Name', position: 'Job Title / Position', description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio, eveniet repellat! Labore earum ad sed iure a corrupti ratione tenetur quam. Sequi, magni quo. Et explicabo earum fuga illo iure.'})
     setWork(updatedWork)
+    console.log(updatedWork)
   }
 
+  //clear input fields and preview
   const clearPreview = () => {
     setName("Your Name"), setEmail("you@yourdomain.com");
     setContact("123-456-7890");
@@ -71,21 +76,21 @@ export default function MainPanel() {
 
         <h2>Education</h2>
         <hr></hr>
-        {education.map((edu, index) => (
-          <div key={index}>
+        {education.map((educ) => (
+          <div key={educ.id}>
             <div className="educ-info">
-              <div><span>{edu.from}</span> - <span>{edu.to}</span></div>
-              <p><b>{edu.school}</b></p>
-              <p>{edu.field}</p>
+              <div><span>{educ.from}</span> - <span>{educ.to}</span></div>
+              <p><b>{educ.school}</b></p>
+              <p>{educ.field}</p>
             </div>
           </div>
         ))}
 
         <h2>Work Experience</h2>
         <hr></hr>
-        {work.map((work, index) => (
-          <div className="work-info" key={index}>
-            <div className="educ-info">
+        {work.map((work) => (
+          <div className="work-info-wrapper" key={work.id}>
+            <div className="work-info">
               <div><span>{work.from}</span> - <span>{work.to}</span></div>
               <p><b>{work.company}</b></p>
               <p>{work.position}</p>
